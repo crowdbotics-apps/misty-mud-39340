a09b06b16406ac8b0658d72f6ad56a3eb30b4182
Crowdbotics <team@crowdbotics.com>
2023-03-12 20:52:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/misty_mud_39340/MainActivity.java
@@ -0,0 +1,15 @@
+package com.misty_mud_39340;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "misty_mud_39340";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "misty-mud-39340",
+  url: "https://misty-mud-39340.botics.co",
+  api: "https://misty-mud-39340.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
